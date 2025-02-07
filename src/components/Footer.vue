4ec5027363becd2f0560beb8cf6dd78230dc17b3
LONG HÀ <154896404+longhangoc@users.noreply.github.com>
2025-02-07 23:08:18 +0700

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,13 +2,13 @@
   <footer class="modern-footer">
     <div class="footer-content">
       <div class="contact-buttons">
-        <a href="#" class="contact-button facebook">
+        <a href="https://www.facebook.com/share/1KTNPuLy7V/" class="contact-button facebook">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
             <path d="M12 2C6.477 2 2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.879V14.89h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.989C18.343 21.129 22 16.99 22 12c0-5.523-4.477-10-10-10z"/>
           </svg>
           <span>Facebook</span>
         </a>
-        <a href="#" class="contact-button telegram">
+        <a href="https://t.me/longha102" class="contact-button telegram">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
             <path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm4.888 7.405l-1.566 7.338c-.116.511-.406.636-.822.395l-2.369-1.749-1.137 1.099c-.125.125-.23.23-.472.23l.168-2.395 4.361-3.941c.189-.168-.042-.263-.285-.094l-5.389 3.39-2.321-.724c-.504-.159-.513-.504.106-.743l9.054-3.49c.424-.153.798.105.659.739z"/>
           </svg>
@@ -16,7 +16,7 @@
         </a>
       </div>
       <div class="footer-bottom">
-        <p>© {{ currentYear }} LONGHA. Đã đăng ký bản quyền.</p>
+        <p>© {{ currentYear }} LONGHA.</p>
       </div>
     </div>
   </footer>
