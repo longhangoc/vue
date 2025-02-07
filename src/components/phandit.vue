fe4ed5aa026dbb8b521260703f188da8cc681c33
LONG HÀ <154896404+longhangoc@users.noreply.github.com>
2025-02-07 22:03:34 +0700

    Create phandit.vue

---
+++ src/components/phandit.vue
@@ -0,0 +1,39 @@
+<template>
+  <footer class="footer">
+    <p>© 2025 HAL. All rights reserved.</p>
+    <div class="social-icons">
+      <a href="#"><i class="fab fa-facebook"></i></a>
+      <a href="#"><i class="fab fa-twitter"></i></a>
+      <a href="#"><i class="fab fa-instagram"></i></a>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'phandit'
+}
+</script>
+
+<style scoped>
+.footer {
+  background-color: #34495e;
+  color: white;
+  text-align: center;
+  padding: 20px;
+  position: relative;
+  bottom: 0;
+  width: 100%;
+}
+
+.social-icons a {
+  color: white;
+  font-size: 20px;
+  margin: 0 10px;
+  transition: color 0.3s;
+}
+
+.social-icons a:hover {
+  color: #42b983;
+}
+</style>
