adf3debda03bb9e3d34c7e561c7078e79a882dce
LONG HÀ <154896404+longhangoc@users.noreply.github.com>
2025-02-07 22:04:12 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <phandau />
+    <div class="content">
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+    <phandit />
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import phandau from './components/phandau.vue'
+import phandit from './components/phandit.vue'
 
 export default {
   name: 'App',
   components: {
+    phandau,
+    phandit,
     HelloWorld
   }
 }
@@ -20,6 +30,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  height: 100vh; /* Chiều cao full màn hình */
+}
+
+.content {
+  flex: 1; /* Chiếm hết phần còn lại giữa header và footer */
+  display: flex;
+  justify-content: center; /* Căn giữa theo chiều ngang */
+  align-items: center; /* Căn giữa theo chiều dọc */
 }
 </style>
